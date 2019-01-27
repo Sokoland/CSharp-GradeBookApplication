1e1a02e572a0e75c725d3d4cda20e353b0475236
Aleksander Dąbrowski <aledab406@hotmail.com>
2019-01-27 21:08:29 +0100

    Overrided RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,35 @@ namespace GradeBook.GradeBooks
         {
             this.Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            List<Student> tempStudentList = new List<Student>();
+            foreach (Student student in Students)
+                tempStudentList.Add(student);
+            for (int i = 0; i < tempStudentList.Count-1; i++)
+            {
+                if((tempStudentList[i].AverageGrade<tempStudentList[i+1].AverageGrade))
+                {
+                    Student studentHolder = tempStudentList[i];
+                    tempStudentList[i] = tempStudentList[i + 1];
+                    tempStudentList[i + 1] = studentHolder;
+                   
+                }
+            }
+
+            if (averageGrade > tempStudentList[(int)Math.Ceiling(tempStudentList.Count * 0.2)].AverageGrade)
+                return 'A';
+            else if (averageGrade > tempStudentList[(int)Math.Ceiling(tempStudentList.Count * 0.4)].AverageGrade)
+                return 'B';
+            else if (averageGrade > tempStudentList[(int)Math.Ceiling(tempStudentList.Count * 0.6)].AverageGrade)
+                return 'C';
+            else if (averageGrade > tempStudentList[(int)Math.Ceiling(tempStudentList.Count * 0.8)].AverageGrade)
+                return 'D';
+            else /*if (averageGrade >= tempStudentList[tempStudentList.Count].AverageGrade)*/
+                return 'F';
+           
+        }
     }
 }
